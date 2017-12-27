876381432b977cae8a87320a665d43214fc3336c
Vishwa Prasad <vishwa-6077@csez.zohocorpin.com>
2017-12-27 15:11:33 +0530

    Parking Reservation System

---
+++ Parking Reservation System/src/Floor.java
@@ -0,0 +1,98 @@
+import java.util.HashMap;
+
+public class Floor {
+	private HashMap slots = new HashMap();
+	private int cref = 100, eref = 1, vref = 10, rref = 55;
+	private int cst=100,cend=399,est=1,eend=9,vst=10,vend=54,rst=55,rend=99;
+	
+	public HashMap getSlots() {
+		return slots;
+	}
+
+	public int getCst() {
+		return cst;
+	}
+	
+	public void setCst(int cst) {
+		this.cst = cst;
+	}
+
+	public int getEst() {
+		return est;
+	}
+
+	public int getVst() {
+		return vst;
+	}
+
+	public int getRst() {
+		return rst;
+	}
+	
+	public Integer addCustomer() {
+		int empty_slot = checkEmptySlots(cref,cst-1);
+		if( empty_slot != -1) {
+			return empty_slot;
+		}
+		else if(cst<=cend) {
+			return cst;
+		}
+		else
+			return -1;
+	}
+
+
+	public Integer addEmergency() {
+		int empty_slot = checkEmptySlots(eref,est-1);
+		if( empty_slot != -1) {
+			slots.replace(empty_slot, "activated");
+			return empty_slot;
+		}
+		else if(est<=eend) {
+			slots.put(est,"activated");
+			est++;
+			return est-1;
+		}
+		else
+			return -1;
+	}
+	
+	public Integer addVIP() {
+		int empty_slot = checkEmptySlots(vref,vst-1);
+		if( empty_slot != -1) {
+			slots.replace(empty_slot, "activated");
+			return empty_slot;
+		}
+		else if(vst<=vend) {
+			slots.put(vst,"activated");
+			vst++;
+			return vst-1;
+		}
+		else
+			return -1;
+	}
+	
+	public Integer addReserved() {
+		int empty_slot = checkEmptySlots(rref,rst-1);
+		if( empty_slot != -1) {
+			slots.replace(empty_slot, "activated");
+			return empty_slot;
+		}
+		else if(rst<=rend) {
+			slots.put(rst,"activated");
+			rst++;
+			return rst-1;
+		}
+		else
+			return -1;
+	}
+	
+	private Integer checkEmptySlots(int st, int end){
+		for(int i=st;i<=end;i++) {
+			if(this.slots.get(i).equals("deactivated"))
+				return i;
+		}
+		return -1;
+	}
+	
+}
---
+++ Parking Reservation System/src/Index.java
@@ -0,0 +1,147 @@
+import java.util.*;
+
+public class Index {
+	public static void main(String args[]) {
+		
+		Scanner input = new Scanner(System.in);
+		Basement b = new Basement();
+		Floor f = new Floor();
+		int ch,c = 0;
+		
+		System.out.println("--------------------------------");
+		System.out.println(":: Parking Reservation System ::");
+		System.out.println("--------------------------------");
+		while(c == 0) {
+			System.out.println(":: 1.Book Parking 2.Checkout 3.Floor Slots 4.Basement Slots 5.Exit ::");
+			ch= input.nextInt();
+			switch(ch) {
+				case 1: checkIn(f,b,input); break;
+				case 2: checkOut(f,b,input); break;
+				case 3: System.out.println(f.getSlots().toString()); break;
+				case 4: System.out.println(b.getSlots().toString()); break;
+				default:
+					c=1;
+					System.out.println("--------------------------------");
+					System.out.println(":: Thank you. Have a nice day! ::");
+					System.out.println("--------------------------------");
+			}
+		}
+	}
+	
+	public static void checkIn(Floor f, Basement b, Scanner input) {
+		System.out.println("-----------------------------------------------------------");
+		System.out.println(":: 1.Customer 2.Emergency 3.VIP 4.Reserved 5. Shop Owner ::");
+		System.out.println("-----------------------------------------------------------");
+		int ch = input.nextInt();
+		int alloted_slot,fcustomer_slot,bcustomer_slot;
+		switch(ch) {
+			case 1 : 
+					fcustomer_slot = f.addCustomer();
+					bcustomer_slot = b.addCustomer();
+					// To decide between basement and floor
+					parkCustomer(fcustomer_slot,bcustomer_slot,f,b);
+					break;
+			case 2 : 
+					 alloted_slot = f.addEmergency();
+					 if(alloted_slot != -1)
+						System.out.println("Parking slot number is F"+ alloted_slot);
+					 else {
+						 System.out.println("Parking Full.");
+					 }
+					 	break;  
+			case 3 : 
+					 alloted_slot = f.addVIP();
+					 if(alloted_slot != -1)
+						System.out.println("Parking slot number is F"+ alloted_slot);
+					 else {
+						 System.out.println("Parking Full.");
+					 }
+					 	break;  
+			case 4 : 
+					 alloted_slot = f.addReserved();
+					 if(alloted_slot != -1)
+						System.out.println("Parking slot number is F"+ alloted_slot);
+					 else {
+						 System.out.println("Parking Full.");
+					 }
+					 	break; 
+			case 5 : 
+					 alloted_slot = b.addSales();
+					 if(alloted_slot != -1)
+						System.out.println("Parking slot number is B"+ alloted_slot);
+					 else {
+						 System.out.println("Parking Full.");
+					 }
+					 	break;
+			}	
+	  }
+	
+	public static void checkOut(Floor f, Basement b, Scanner input) {
+		System.out.println("--------------------------------");
+		System.out.println(":: 1. Basement 2.Floor 3.Exit ::");
+		System.out.println("--------------------------------");
+		int ch = input.nextInt();
+		int slotno;
+		switch(ch) {
+			case 1: System.out.println(":: Enter the slot number ::");
+				    slotno = input.nextInt();
+				    b.getSlots().replace(slotno, "deactivated");
+					System.out.println(b.getSlots().toString());
+					break;
+			case 2: System.out.println(":: Enter the slot number ::");
+					slotno = input.nextInt();
+					f.getSlots().replace(slotno, "deactivated");
+					System.out.println(f.getSlots().toString());
+					break;
+		
+		}
+	}
+	
+	public static void parkCustomer(int fcustomer_slot, int bcustomer_slot, Floor f, Basement b) {
+		if((fcustomer_slot!= -1) && (fcustomer_slot <= bcustomer_slot)) {
+			if(fcustomer_slot == f.getCst()) {
+				f.getSlots().put(fcustomer_slot, "activated");
+				f.setCst(fcustomer_slot+1);
+			}
+			else {
+				f.getSlots().replace(fcustomer_slot, "activated");
+			}
+			System.out.println("Parking slot number is F"+ fcustomer_slot);
+		}
+		else if((bcustomer_slot!= -1) && (fcustomer_slot > bcustomer_slot)) {
+			if(bcustomer_slot == b.getCst()) {
+				b.getSlots().put(bcustomer_slot, "activated");
+				b.setCst(bcustomer_slot+1);
+			}
+			else {
+				b.getSlots().replace(bcustomer_slot, "activated");
+			}
+			System.out.println("Parking slot number is B"+ bcustomer_slot);
+		}
+		else {
+			 if(fcustomer_slot != -1 && bcustomer_slot == -1) {
+				 if(fcustomer_slot == f.getCst()) {
+						f.getSlots().put(fcustomer_slot, "activated");
+						f.setCst(fcustomer_slot+1);
+					}
+				else {
+					f.getSlots().replace(fcustomer_slot, "activated");
+				}
+				System.out.println("Parking slot number is F"+ fcustomer_slot);
+			 }
+			 else if(bcustomer_slot != -1 && fcustomer_slot == -1) {
+				 if(bcustomer_slot == b.getCst()) {
+						b.getSlots().put(bcustomer_slot, "activated");
+						b.setCst(bcustomer_slot+1);
+					}
+				else {
+					b.getSlots().replace(bcustomer_slot, "activated");
+				}
+				System.out.println("Parking slot number is B"+ bcustomer_slot);
+			 }
+			 else {	 
+				 System.out.println("Parking Full.");
+			 }
+		 }
+	}
+}
